a7e10d6c9747924403f21d49a0b54af7fbcb6954
Roman Princ <romanruins08@hotmail.com>
2021-07-24 17:54:46 -0700

    "Added a command to get player stats from api, very jank"

--- venv/cogs/NHLBot.py
+++ venv/cogs/NHLBot.py
@@ -1,14 +1,17 @@
 import discord
 from discord.ext import commands
 import aiohttp
+import json
 
 
 class Player:
-    def __init__(self, firstName, lastName, round, pick):
+    def __init__(self, firstName, lastName, position, round, pick, team):
         self.firstName = firstName
         self.lastName = lastName
+        self.position = position
         self.round = round
         self.pick = pick
+        self.team = team
 
 class NHLBot(commands.Cog):
     def __init__(self, bot):
@@ -69,30 +72,37 @@ class NHLBot(commands.Cog):
             async with cs.get(url) as r:
                 res = await r.json()  # returns dict
                 for key in res.items():
-                        for values in key:
-                            if values == "data" or values == "total" or type(values) is int:
-                                pass
-                            else:
-                                for player in values:
-                                    x = Player(player['firstName'], player['lastName'], player['roundNumber'], player['overallPickNumber'])
-                                    player_list.append(x)
+                    for values in key:
+                        if values == "data" or values == "total" or type(values) is int:
+                            pass
+                        else:
+                            for player in values:
+                                print(player)
+                                x = Player(player['firstName'], player['lastName'], player['position'], player['roundNumber'], player['overallPickNumber'], player['teamPickHistory'])
+                                player_list.append(x)
 
         players = ""
-        for player in player_list:
-            players += f"{player.firstName} {player.lastName}: pick {player.pick}: round {player.round}" + "\n"
-
 
         if team == None:
+            for player in player_list:
+                players += f"{player.position} {player.firstName} {player.lastName}: pick {player.pick}: {player.team}" + "\n"
+
             embed = discord.Embed(
                 title=f"{year} NHL Draft: Round {round}",
                 description=players
             )
         elif round == 0:
+            for player in player_list:
+                players += f"{player.position} {player.firstName} {player.lastName}: pick {player.pick}: round {player.round}: {player.team}" + "\n"
+
             embed = discord.Embed(
                 title=f"{team} {year} NHL Draft",
                 description=players
             )
         else:
+            for player in player_list:
+                players += f"{player.position} {player.firstName} {player.lastName}: pick {player.pick}" + "\n"
+
             embed = discord.Embed(
                 title=f"{team} {year} draft round {round}",
                 description=players
@@ -103,6 +113,77 @@ class NHLBot(commands.Cog):
     @draft.error
     async def draft_error(self, ctx, error):
         await ctx.send(error)
+
+    def Convert(lst):
+        res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+        return res_dct
+
+    @nhl.command()
+    async def player(self, ctx, name, team, season_start_year: int, season_end_year: int, type: str):
+
+        teamID = await self.getTeamID(team, season_start_year)
+        season = str(season_start_year) + str(season_end_year)
+        team_url = f"https://statsapi.web.nhl.com/api/v1/teams/{teamID}?expand=team.roster&season={season}"
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(team_url) as r:
+                res = await r.json()
+                for items in res['teams']:
+                    for items2 in items['roster']['roster']:
+                        if name == items2['person']['fullName']:
+                            print(items2['position'])
+                            player_id = items2['person']['id']
+                            position = items2['position']['code']
+                            break
+
+        if type == "season":
+            player_url = f"https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats?stats=statsSingleSeason&season={season}"
+        elif type == "career":
+            player_url = f"https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats?stats=yearByYear"
+
+        goals = 0
+        assists = 0
+        wins = 0
+        losses = 0
+        save_percentage = []
+        goals_against_average = []
+
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(player_url) as r:
+                res = await r.json()
+                for items in res['stats']:
+                    for items2 in items['splits']:
+                        if type == "career":
+                            if items2['league']['name'] == "NHL" or items2['league']['name'] == "National Hockey League":
+                                if position == "G":
+                                    print(items2['stat'])
+                                    wins += items2['stat']['wins']
+                                    losses += items2['stat']['losses']
+                                    save_percentage.append(items2['stat']['savePercentage'])
+                                    goals_against_average.append(items2['stat']['goalAgainstAverage'])
+                                else:
+                                    goals += items2['stat']['goals']
+                                    assists += items2['stat']['assists']
+                        else:
+                            if position == "G":
+                                print(items2['stat'])
+                                wins += items2['stat']['wins']
+                                losses += items2['stat']['losses']
+                                save_percentage.append(items2['stat']['savePercentage'])
+                                goals_against_average.append(items2['stat']['goalAgainstAverage'])
+                            else:
+                                goals += items2['stat']['goals']
+                                assists += items2['stat']['assists']
+
+
+        if position == "G":
+            average_svp = sum(save_percentage) / len(save_percentage)
+            average_gaa = sum(goals_against_average) / len(goals_against_average)
+            await ctx.send(f"wins: {wins} losses: {losses} save percentage: {round(average_svp, 3)} GAA: {round(average_gaa, 2)} Note: SVP and GAA slighly off from actual values due to API issues")
+        else:
+            points = goals + assists
+            await ctx.send(f"goals: {goals} assists: {assists} points: {points}")
+
+
 
 
 def setup(bot):
